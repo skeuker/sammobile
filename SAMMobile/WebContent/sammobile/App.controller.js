74824fca805ce92bfcab3b27b92bad993d0e256e
skeuker <stefan@rbgapps.com>
2016-06-15 23:37:42 +0300

    Attempted push to remote

--- SAMMobile/WebContent/sammobile/App.controller.js
+++ SAMMobile/WebContent/sammobile/App.controller.js
@@ -6,7 +6,7 @@ sap.ui.controller("sammobile.App", {
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf sammobile.App
+* @memberOf sammobile.App 
 */
 	onInit: function() {
 		
