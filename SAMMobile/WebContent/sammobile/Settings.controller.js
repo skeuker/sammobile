61154ba24f0bf35057caadcffd63a2c34538047a
skeuker <stefan@rbgapps.com>
2016-06-21 18:51:52 +0300

    Demo change

--- SAMMobile/WebContent/sammobile/Settings.controller.js
+++ SAMMobile/WebContent/sammobile/Settings.controller.js
@@ -20,6 +20,7 @@ sap.ui.controller("sammobile.Settings", {
 //
 //	},
 
+
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
